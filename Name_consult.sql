56419d4cd959777adbd35f14c5455e4eca49f21c
william100690 <william.vilchis.personal@gmail.com>
2023-04-28 20:40:55 -0600

    This file shows how to make single query

---
+++ Name_consult.sql
@@ -0,0 +1,13 @@
+USE computer_store;
+
+-- list the name of all the products that are in the product table
+SELECT NAME from product;
+
+
+-- list the name and the prices of all the products from product table
+SELECT name, price from product;
+
+
+-- list all the columns from the product table
+SELECT * from product;
+
